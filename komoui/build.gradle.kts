27c6875d356d54f68d71e60eb17fde6585a300b9
derangga <derangga1011@gmail.com>
2026-07-10 17:41:05 +0700

    feat: bump some dependency version

--- komoui/build.gradle.kts
+++ komoui/build.gradle.kts
@@ -40,12 +40,6 @@ kotlin {
     // https://developer.android.com/kotlin/multiplatform/migrate
     val xcfName = "KomoUI"
 
-    iosX64 {
-        binaries.framework {
-            baseName = xcfName
-        }
-    }
-
     iosArm64 {
         binaries.framework {
             baseName = xcfName
